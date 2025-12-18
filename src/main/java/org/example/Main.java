bb438b1934ba931b43467621b01c5092f9cdaf27
Jonas Honorato <jonashonorato77@gmail.com>
2025-12-18 09:38:13 -0300

    Reorganiza projeto e remove código não utilizado

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Moto m = new Moto("honda","Azul", 2000);
-        Carro carro = new Carro("subaru","azul",1500);
-        carro.acelerar();
-    }
-}
